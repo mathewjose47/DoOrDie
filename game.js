84d5f25d0dd5b478f783b2c8d698d3560017d558
AkashG6 <akashgarule@gmail.com>
2017-01-18 22:17:05 +0530

    Add files via upload
    added up timer in bottom left

--- game.js
+++ game.js
@@ -68,7 +68,46 @@ gameState2.prototype={
 
     preload : preload2,
     create : create2,
-    update : update2
+    update : update2,
+
+    createTimer: function(){
+ 
+    var me = this;
+    
+    me.timeLabel = me.game.add.text(100, 640, "00:00", {font: "100px Arial", fill: "#fff"}); 
+    me.timeLabel.anchor.setTo(0.5, 0);
+    me.timeLabel.align = 'center';
+    me.timeLabel.scale.setTo(0.4,0.4);
+
+ 
+    },
+    updateTimer: function(){
+ 
+    var me = this;
+ 
+    var currentTime = new Date();
+    var timeDifference = me.startTime.getTime() - currentTime.getTime();
+ 
+    //Time elapsed in seconds
+    me.timeElapsed = Math.abs(timeDifference / 1000);
+ 
+    //Time remaining in seconds
+    //var timeRemaining = me.totalTime - me.timeElapsed; 
+    var timeRemaining = me.timeElapsed;
+ 
+    //Convert seconds into minutes and seconds
+    var minutes = Math.floor(timeRemaining / 60);
+    var seconds = Math.floor(timeRemaining) - (60 * minutes);
+ 
+    //Display minutes, add a 0 to the start if less than 10
+    var result = (minutes < 10) ? "0" + minutes : minutes; 
+ 
+    //Display seconds, add a 0 to the start if less than 10
+    result += (seconds < 10) ? ":0" + seconds : ":" + seconds; 
+ 
+    me.timeLabel.text = result;
+ 
+    }
 };
 
 gameState3.prototype={
@@ -266,6 +305,19 @@ function goFull() {
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
+
+    // UP COUNTER
+    var me = this;
+ 
+    me.startTime = new Date();
+    me.totalTime = 120;
+    me.timeElapsed = 0;
+ 
+    me.createTimer();
+ 
+    me.gameTimer = game.time.events.loop(100, function(){
+        me.updateTimer();
+    });
 
 
 
